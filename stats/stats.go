51412db0bce3478daeaf5c45b1dd4ae001d9906e
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2018-01-25 16:20:10 +0000

    Temprorarily disable stats

--- stats/stats.go
+++ stats/stats.go
@@ -1,8 +1,6 @@
 package stats
 
 import (
-	"strings"
-
 	"github.com/botanio/sdk/go"
 	"github.com/w32blaster/bot-tfl-next-departure/structs"
 	"gopkg.in/telegram-bot-api.v4"
@@ -15,25 +13,25 @@ type BotanCommand struct {
 // TrackMessage sends statistics about a message to the Botan.io
 func TrackMessage(message *tgbotapi.Message, botanToken string) {
 
-	tokens := strings.Fields(message.Text)
-	command := tokens[0]
+	//tokens := strings.Fields(message.Text)
+	//command := tokens[0]
 
 	// track it
-	_track(botanToken, message.From.ID, BotanCommand{tokens}, command)
+	//_track(botanToken, message.From.ID, BotanCommand{tokens}, command)
 }
 
 // TrackJourney track a bookmark showing
 func TrackJourney(journey *structs.JourneyRequest, userID int, botanToken string) {
 
 	// track it
-	_track(botanToken, userID, journey, "ShowJourney")
+	//_track(botanToken, userID, journey, "ShowJourney")
 }
 
 // TrackError track an error
 func TrackError(err error, userFrom int, botanToken string) {
 
 	// track it
-	_track(botanToken, userFrom, err.Error(), "Error")
+	//_track(botanToken, userFrom, err.Error(), "Error")
 }
 
 // common method to send track asynchronousely
